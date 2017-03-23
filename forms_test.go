aa0a4b3920bfccc8d12d5ddadfce30d54cc41606
runner.mei <runner.mei@gmail.com>
2017-03-23 19:09:56 +0800

    fix some compile error

--- forms_test.go
+++ forms_test.go
@@ -19,7 +19,7 @@ func makeCtx() interface{} {
 
 func TestFieldRender(t *testing.T) {
 	field := TextField(makeCtx(), "test", "test")
-	field.AddClass("test").AddClass("class").SetId("testId").SetParam("param1", "val1").AddCss("css1", "val1").SetStyle(style).Disabled()
+	field.AddClass("test").AddClass("class").SetID("testId").SetParam("param1", "val1").AddCss("css1", "val1").SetStyle(style).Disabled()
 	field.AddLabelClass("LABEL")
 	field.SetLabel("This is a label")
 	field.AddError("ERROR")
@@ -31,7 +31,7 @@ func TestPasswordRender(t *testing.T) {
 	field := PasswordField(makeCtx(), "test", "test")
 	field.AddClass("test")
 	field.AddClass("class")
-	field.SetId("testId")
+	field.SetID("testId")
 	field.SetStyle(style)
 	field.SetValue("asd")
 	t.Log("Rendered field:", field.Render())
@@ -46,16 +46,16 @@ func TestButtonRender(t *testing.T) {
 }
 
 func TestRadioButtonRender(t *testing.T) {
-	field := RadioField(makeCtx(), "radio", []InputChoice{
-		InputChoice{Id: "choice1", Val: "value1"},
-		InputChoice{Id: "choice2", Val: "value2"},
+	field := RadioField(makeCtx(), "radio", "select one", []InputChoice{
+		InputChoice{ID: "choice1", Val: "value1"},
+		InputChoice{ID: "choice2", Val: "value2"},
 	})
 	field.SetStyle(style)
 	t.Log("Rendered radio:", field.Render())
 }
 
 func TestSelectRender(t *testing.T) {
-	field := SelectField(makeCtx(), "select", map[string][]InputChoice{
+	field := SelectField(makeCtx(), "select", "select one", map[string][]InputChoice{
 		"": []InputChoice{
 			InputChoice{"choice1", "value1"},
 			InputChoice{"choice2", "value2"},
@@ -72,7 +72,7 @@ func TestHiddenRender(t *testing.T) {
 }
 
 func TestNumberRender(t *testing.T) {
-	field := NumberField(makeCtx(), "number")
+	field := NumberField(makeCtx(), "number", "input number")
 	field.SetStyle(style)
 	t.Log("Rendered number:", field.Render())
 }
@@ -103,7 +103,7 @@ func TestFormFromModel(t *testing.T) {
 	type Model struct {
 		User      string    `form_label:"User label test"`
 		password  string    `form_widget:"password"`
-		Id        int       `form_min:"0" form_max:"5"`
+		ID        int       `form_min:"0" form_max:"5"`
 		Ts        time.Time `form_min:"2013-04-22T15:00"`
 		RadioTest string    `form_widget:"select" form_choices:"|A|Option A|G1|B|Option B" form_value:"A"`
 		BoolTest  bool      //`form_options:"checked"`
@@ -117,7 +117,7 @@ func TestBSFormFromModel(t *testing.T) {
 	type Model struct {
 		User      string    `form_label:"User label test"`
 		password  string    `form_widget:"password"`
-		Id        int       `form_min:"0" form_max:"5"`
+		ID        int       `form_min:"0" form_max:"5"`
 		Ts        time.Time `form_min:"2013-04-22T15:00"`
 		RadioTest string    `form_widget:"select" form_choices:"|A|Option A|G1|B|Option B" form_value:"A"`
 		BoolTest  bool      //`form_options:"checked"`
@@ -129,12 +129,12 @@ func TestBSFormFromModel(t *testing.T) {
 
 func TestInlineCreation(t *testing.T) {
 	form := BaseForm(POST, "/action.html").Elements(
-		TextField("text_field").SetLabel("Username"),
+		TextField(makeCtx(), "text_field", "Username"),
 		FieldSet("psw_fieldset",
-			PasswordField("psw1").AddClass("password_class").SetLabel("Password 1"),
-			PasswordField("psw2").AddClass("password_class").SetLabel("Password 2"),
+			PasswordField(makeCtx(), "psw1", "Password 1").AddClass("password_class"),
+			PasswordField(makeCtx(), "psw2", "Password 2").AddClass("password_class"),
 		),
-		SubmitButton("btn1", "Submit"),
+		SubmitButton(makeCtx(), "btn1", "Submit"),
 	)
 	t.Log("Rendered form:", form.Render())
 }
@@ -142,12 +142,12 @@ func TestInlineCreation(t *testing.T) {
 func TestPizzaCreation(t *testing.T) {
 
 	type Ingredient struct {
-		IngredientId int    `db_key:"true" db_autoincrement:"true"`
+		IngredientID int    `db_key:"true" db_autoincrement:"true"`
 		Name         string `db_size:"30"`
 	}
 
 	type Pizza struct {
-		Id   int    `db_key:"true" db_autoincrement:"true"`
+		ID   int    `db_key:"true" db_autoincrement:"true"`
 		Name string `db_size:"30"`
 		// Ingredients []int
 		Price float32 `form_widget:"number" form_min:"0"`
